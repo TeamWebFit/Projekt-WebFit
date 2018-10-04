2bf4f0e50c8ea72572004115f0952edc6d4f0e7d
Kathi <katharina@ussling.net>
2018-10-04 17:13:39 +0200

    Registrierung klappt

--- src/App.js
+++ src/App.js
@@ -10,12 +10,37 @@ import Welcome from './components/Welcome';
 import notfound from './pages/404';
 import profil from './pages/user';
 import login from './pages/Login';
+import Register from './pages/Register';
+import * as qs from 'query-string';
+import Alert from './components/Alert';
 
 // Don't link any component (except Header & Footer)
 // Please include new pages through react-router here
 
+const checkRegistrationStatus = () => {
+  if(status.registration == "true"){
+    alert("Registrierung erfolgreich")
+  }
+}
+
+const queryString = require('query-string');
+const status = queryString.parse(window.location.search);
+console.log(status.registration);
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      registrationStatus: ''
+    };
+  }
+
+  componentWillMount(){
+    console.log('componentWillMount');
+    checkRegistrationStatus();
+  }
+
   render() {
     return (
       <Router>
@@ -27,7 +52,8 @@ class App extends Component {
               <Route path="/demo" component={Demo} />
               <Route path="/login" component={login} />
               <Route path="/user" component={profil} />
-              
+              <Route path="/register" component={Register} />
+              <div id="registrationTrue"></div>
             {/* React-Router - Route End */}
             </div>
         <Footer />
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -8,21 +8,19 @@ import { withRouter, Link } from 'react-router-dom';
 //Apollo
 
 
-
 class Register extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      //gender: '',
+      firstName: '',
       name: '',
-      //nachname:  '',
       email: '',
       password: '',
       //tag: '',
       //monat: '',
       //jahr: '',
-      //gender: ''
+      gender: ''
     };
 
     this.onChange = this.onChange.bind(this);
@@ -36,20 +34,22 @@ class Register extends Component {
   onSubmit(e){
     e.preventDefault();
     console.log("OnSubmit funktioniert");
-
-    const user = {
-      name: this.state.name,
-      email: this.state.email,
-      password: this.state.password
-    };
-
-    this.props.newUserMutation(user);
+    var gen = parseInt(this.state.gender);
+    this.props.newUserMutation({
+      variables: {
+        firstName: this.state.firstName,
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password,
+        gender: gen
+      }
+    })
+    console.log("Submit fertiiiig");
+    this.props.history.push("/?registration=true");
   }
 
   render(){
-
-
-
+    console.log(this.props);
     {/*const userToRender = this.props.allUsersQuery.allTestUsers
     console.log(userToRender);*/}
 
@@ -59,13 +59,13 @@ class Register extends Component {
           <div className="card-body">
             <h3>Jetzt bei WebFit registrieren!</h3>
             <br />
-            <form> {/*onSubmit={this.onSubmit}*/}
+            <form onSubmit={this.onSubmit}> {/*onSubmit={this.onSubmit}*/}
                 <div className="form-group">
-                  <input type="text" className="form-control" id="InputName" placeholder="name" name="name" onChange={this.onChange} value={this.state.name} />
+                  <input type="text" className="form-control" id="InputName" placeholder="vorname" name="firstName" onChange={this.onChange} value={this.state.firstName} />
                 </div>
-                {/*<div className="form-group">
-                  <input type="text" className="form-control" id="InputNachname" placeholder="nachname" name="nachname" onChange={this.onChange} value={this.state.nachname} />
-                </div>*/}
+                <div className="form-group">
+                  <input type="text" className="form-control" id="InputNachname" placeholder="nachname" name="name" onChange={this.onChange} value={this.state.name} />
+                </div>
                 <div className="form-group">
                   <input type="email" className="form-control" id="InputEmail" placeholder="email" name="email" onChange={this.onChange} value={this.state.email} />
                 </div>
@@ -84,48 +84,51 @@ class Register extends Component {
                   <select className="custom-select my-1 mr-sm-2 col-md-4" name="jahr" onChange={this.onChange} value={3}>
                     <option>1990</option>
                   </select>
-                </div>
+                </div>*/}
                 <br />
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="gender" id="Radio1" onChange={this.onChange} value={"1"} />
+                  <input className="form-check-input" type="radio" name="gender" onChange={this.onChange} value={1} />
                   <label className="form-check-label"> weiblich </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="gender" id="Radio2" onChange={this.onChange} value={"0"} />
+                  <input className="form-check-input" type="radio" name="gender" onChange={this.onChange} value={0} />
                   <label className="form-check-label"> männlich </label>
                 </div>
                 <div className="form-check">
-                  <input className="form-check-input" type="radio" name="gender" id="Radio3" onChange={this.onChange} value={"2"} />
+                  <input className="form-check-input" type="radio" name="gender" onChange={this.onChange} value={2} />
                   <label className="form-check-label"> divers </label>
                 </div>
                 <br /><br />
                 <div className="form-group form-check">
-                  <input type="checkbox" className="form-check-input" id="Check1" required={true} onChange={this.onChange} />
+                  <input type="checkbox" className="form-check-input" id="Check1" required={true} />
                   <label className="form-check-label">AGBs</label>
                 </div>
                 <div className="form-group form-check">
-                  <input type="checkbox" className="form-check-input" id="Check2" required={true} onChange={this.onChange} />
+                  <input type="checkbox" className="form-check-input" id="Check2" required={true} />
                   <label className="form-check-label">Datenschutzbedingungen</label>
-                </div>*/}
-                <button className="btn btn-primary" onClick={() => this._createTestUser()}>Submit</button>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button> {/*onClick={() => this._createUser()}*/}
             </form>
           </div>
         </div>
       </div>
     )//end return
+
   }//End Render
 
-  _createTestUser = async () => {
-     console.log("in Funktion");
-    const { name, email, password } = this.state
+  _createUser = async () => {
+    console.log("Start Datenübertragung");
+    const { firstName, name, email, password, gender } = this.state
     await this.props.newUserMutation({
       variables: {
+        firstName,
         name,
         email,
-        password
+        password,
+        gender
       }
     })
-    console.log("fkt vorbei");
+    console.log("Daten übertragen");
   }
 
 }//End Register Component
@@ -143,15 +146,59 @@ const ALL_USERS_QUERY = gql`
   }
 `
 
-const CREATE_USER_MUTATION = gql`
-mutation NewUserMutation ($name: String!, $email: String!, $password: String!)
-{  createTestUser ( name: $name, email: $email, password: $password)
-  {
-    id
-    name
-    email
-    password }
-}
+const newUserMutation = gql`
+  mutation NewUserMutation($firstName: String!, $name: String!, $email: String!, $password: String!, $gender: Int)
+  {  createUser (
+      firstName: $firstName,
+      name: $name,
+      gender: $gender,
+      authProvider: {
+          email: {
+            email: $email,
+            password: $password
+          }
+      }
+    )
+    {
+      createdAt
+      id
+      firstName
+      name
+      email
+      password
+      gender
+    }
+  }
 `
 //export default graphql(ALL_USERS_QUERY, { name: 'allUsersQuery'})(Register)
-export default graphql(CREATE_USER_MUTATION, { name: 'newUserMutation'})(Register)
+export default graphql(newUserMutation, { name: 'newUserMutation'})(Register)
+
+{/*mutation NewUserMutation($firstName: String!, $name: String!, $email: String, $password: String)
+ {  createUser (
+    firstName: $firstName,
+    name: $name,
+    authProvider: {
+          email: {
+            email: $email,
+            password: $password
+          }
+        }
+    )
+    {
+      createdAt
+      id
+      firstName
+      name
+      email
+      password
+    }
+  }*/}
+
+  {/*mutation NewUserMutation ($name: String!, $email: String!, $password: String!)
+  {  createTestUser ( name: $name, email: $email, password: $password)
+    {
+      id
+      name
+      email
+      password }
+  }*/}
