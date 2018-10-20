a27823d1701433a4009cfb1d77696bc16744fa0a
Kathi <katharina@ussling.net>
2018-10-20 10:46:38 +0200

    Zur Sicherung: Verfiy; token muss noch richtig in Mutation übergeben werden

--- src/pages/Verify.js
+++ src/pages/Verify.js
@@ -5,7 +5,8 @@ import '../App.css';
 //Apollo
 import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
-import { Query } from "react-apollo";
+import { Query } from 'react-apollo';
+
 //Router
 import { withRouter, Link } from 'react-router-dom';
 
@@ -14,8 +15,10 @@ import { withRouter, Link } from 'react-router-dom';
 // Verify Component
 // for Double-Opt-In: after confirmation Link
 // ===================
+
 const queryString = require('query-string');
-var parsed, userToRender, test;
+var parsed = queryString.parse(window.location.search);
+var token = parsed.token;
 
 class Verify extends Component{
 
@@ -24,52 +27,42 @@ class Verify extends Component{
     this.state = {
       id: '',
       name: '',
-      email: '',
-      authToken: ''
+      email: ''
     }
-    this.onClick = this.onClick.bind(this);
   }
 
-  static getDerivedStateFromProps(props, state){
-    console.log('getDerivedStateFromProps fetchUser');
-    userToRender = props.FetchVerifiedUser.allUsers
-    console.log(userToRender);
-  }
-
-
-  onClick(){
-    var userID = userToRender["0"].id;
-    console.log(userID);
-
-    this.props.UpdateVerifiedUser({
-      variables: {
-        id: userID,
-        active: true
-      }
-    })
-    this.props.history.push("/login");
-  }
 
     render(){
-      parsed = queryString.parse(window.location.search);
-      console.log(parsed.token)
+
+      var userToRender = this.props.FetchVerifiedUser.allUsers;
+      console.log(userToRender);
+
+      if(userToRender){
+        var id = this.props.FetchVerifiedUser.allUsers["0"].id;
+        console.log(id);
+        this.props.UpdateVerifiedUser({
+          variables: {
+            id: id,
+            active: true
+          }
+        })
+      }
 
         return (
             <div className="text-center">
                 <h1>Email-Adresse bestätigt!</h1>
                 <h4>Vielen Dank, du hast Deine Email-Adresse erfolgreich bestätigt.</h4>
-                <button className="btn btn-basic" onClick={this.onClick}>Bestätigen</button>
-
             </div>
         )
+
     }
 }
 
 
 const FetchVerifiedUser = gql`
-  query FetchVerifiedUser{
+  query FetchVerifiedUser($authToken: String){
       allUsers(filter: {
-        authToken:"drz3dcp3xojn0h5leiiomf"
+        authToken: $authToken
       })
       {
         id
@@ -89,7 +82,7 @@ const UpdateVerifiedUser = gql`
 `
 
 export default compose(
-  graphql(FetchVerifiedUser, { name: 'FetchVerifiedUser'}),
-  graphql(UpdateVerifiedUser, { name: 'UpdateVerifiedUser'}),
+  graphql(FetchVerifiedUser, { name: 'FetchVerifiedUser',options: { variables: { authToken: token } }}),
+  graphql(UpdateVerifiedUser, { name: 'UpdateVerifiedUser' }),
   withRouter
 )(Verify);
